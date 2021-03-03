9bff5259c14db4a65c3017840412aeb544c3aa08
Brandt <frickerbrandt@gmail.com>
2021-03-03 11:19:43 -0700

    different data types

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int number = 1;
+        double decimal = 2.99;
+        String str = "dog";
+        double sum = number + decimal;
+        String concat = number + str;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+
+
+
+}
